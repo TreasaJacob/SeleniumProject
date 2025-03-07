818f6162408b3075870a06930b801b324fa6e44e
treas <treas@Stevu>
2025-03-07 15:22:32 +0530

    TestNg tests,dependency and report

---
+++ src/test/java/com/Obsquara/SeleniumProject/TestNgSample/NewTest.java
@@ -0,0 +1,15 @@
+package com.Obsquara.SeleniumProject.TestNgSample;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test(priority = 1)
+  
+  public void sample() {
+	  System.out.println("hello");
+  }
+  @Test(priority = 2)
+  public void eg() {
+	  System.out.println("Hai all");
+  }
+}
